9d65a881d3b1179ae8cd344013cbbb045f36b27c
KulsumRafi <90857133+KulsumRafi@users.noreply.github.com>
2026-02-13 08:19:12 +0000

    Picture add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@ export default function Navbar() {
   return (
     <nav className="fixed w-full bg-dark/80 backdrop-blur-md text-white z-50">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
-        <h1 className="font-bold text-lg tracking-wide">
+        <h1 className="font-open-sans font-bold text-lg tracking-wide">
           Kulsum Rafi
         </h1>
 
         <div className="space-x-8 text-sm">
-          <a href="#about" className="hover:text-accent">About</a>
-          <a href="#skills" className="hover:text-accent">Skills</a>
-          <a href="#projects" className="hover:text-accent">Projects</a>
-          <a href="#contact" className="hover:text-accent">Contact</a>
+          <a href="#about" className="hover:text-accent px-2">About</a>
+          <a href="#skills" className="hover:text-accent px-2">Skills</a>
+          <a href="#projects" className="hover:text-accent px-2">Projects</a>
+          <a href="#contact" className="hover:text-accent px-2">Contact</a>
         </div>
       </div>
     </nav>
